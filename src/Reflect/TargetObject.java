b7a79cdd8b4c2cf35a65bc8fa0f0cd5afc48234a
Minmengtao <2909338187@qq.com>
2021-05-29 15:56:29 +0800

    更新反射

---
+++ src/Reflect/TargetObject.java
@@ -0,0 +1,21 @@
+package Reflect;
+
+/**
+ * @author: minmengtao
+ * @date: 2021/5/29
+ */
+public class TargetObject {
+    private String value;
+
+    public TargetObject() {
+        value = "JavaGuide";
+    }
+
+    public void publicMethod(String s) {
+        System.out.println("I love " + s);
+    }
+
+    private void privateMethod() {
+        System.out.println("value is " + value);
+    }
+}
